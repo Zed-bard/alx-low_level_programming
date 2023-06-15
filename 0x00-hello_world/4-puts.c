44f7c463c9d67db8f4aeedd57e279ed486311656
Zed-bard <zerihunbard@gmail.com>
2023-06-15 00:38:56 -0700

    building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/* This program prints the string "Programming is like building a multilingual puzzle" to standard output. */
+
+#include <stdio.h>
+
+/* The `main` function prints the string "Programming is like building a multilingual puzzle" to standard output. */
+int main() {
+  puts("Programming is like building a multilingual puzzle");
+  return 0;
+}
+
