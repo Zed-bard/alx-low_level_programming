9bdaadc7a3b7e8b9fe758bd281e0a33dfcb19da2
Zerihun Shiferaw <132649828+Zed-bard@users.noreply.github.com>
2023-07-20 14:11:44 -0500

    Delete 0x10-variadic_functions directory

--- 0x10-variadic_functions/variadic_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef _VARIADIC_HEADER
-#define _VARIADIC_HEADER
-
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-#endif
