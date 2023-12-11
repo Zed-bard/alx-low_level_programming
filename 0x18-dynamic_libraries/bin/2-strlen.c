06564d03b42f143dce8e62f58a6c7e2076c495a2
Zed-bard <zerihunbard@gmail.com>
2023-12-11 11:25:53 -0800

    0x18-dynamic_Libraries

---
+++ 0x18-dynamic_libraries/bin/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * getStringLength - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int getStringLength(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
